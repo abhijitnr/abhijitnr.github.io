e4fc587eeecf737e7835d072a3db42e3b62deb15
abhijitnr <abhijitbiswas8859@gmail.com>
2023-03-14 07:23:54 +0530

    toast addedin contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,8 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
+
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 import contact from "../assets/contact.svg";
 
@@ -23,6 +26,7 @@ const Contact = () => {
       .then(
         (result) => {
           console.log(result.text);
+          toast("Email send successfully");
         },
         (error) => {
           console.log(error.text);
@@ -31,101 +35,109 @@ const Contact = () => {
   };
 
   return (
-    <section id="contact">
-      <h1>
-        Contact <span>Info</span>
-      </h1>
+    <>
+      <section id="contact">
+        <h1>
+          Contact <span>Info</span>
+        </h1>
 
-      {/* Contact Info */}
-      <div>
-        {/* Social Link */}
+        {/* Contact Info */}
         <div>
-          <div className="social_icons">
-            <p>
-              <MdCall /> <span>+91 8011545825</span>
-            </p>
-            <p>
-              <IoIosMail /> <span>abhijitbiswas8859@gmail.com</span>
-            </p>
+          {/* Social Link */}
+          <div>
+            <div className="social_icons">
+              <p>
+                <MdCall /> <span>+91 8011545825</span>
+              </p>
+              <p>
+                <IoIosMail /> <span>abhijitbiswas8859@gmail.com</span>
+              </p>
+            </div>
+
+            {/* Social Icons */}
+            <div className="social_icons_contact">
+              {/* Mail */}
+              <a
+                href="mailto:abhijitbiswas8859@gmail.com"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <IoMdMail />
+              </a>
+
+              {/* LinkedIn */}
+              <a
+                href="https://www.linkedin.com/in/abhijit-biswas-3b6586162/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <BsLinkedin />
+              </a>
+
+              {/* Github */}
+              <a
+                href="https://github.com/abhijitnr"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <BsGithub />
+              </a>
+            </div>
+
+            {/* Direct contant me using mail */}
+            <div className="contact_form">
+              <form ref={form} onSubmit={sendEmail}>
+                <label>
+                  Name <span>*</span>
+                </label>
+                <br />
+                <input
+                  type="text"
+                  name="user_name"
+                  placeholder="Name"
+                  required
+                />
+                <br />
+                <label>
+                  Email <span>*</span>
+                </label>
+                <br />
+                <input
+                  type="email"
+                  name="user_email"
+                  placeholder="Email"
+                  required
+                />
+                <br />
+                <label>
+                  Message <span>*</span>
+                </label>
+                <br />
+                <textarea name="message" placeholder="Message" />
+                <br />
+                <input
+                  className="send_button"
+                  type="submit"
+                  value="Send"
+                  required
+                />
+              </form>
+            </div>
           </div>
 
-          {/* Social Icons */}
-          <div className="social_icons_contact">
-            {/* Mail */}
-            <a
-              href="mailto:abhijitbiswas8859@gmail.com"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <IoMdMail />
-            </a>
-
-            {/* LinkedIn */}
-            <a
-              href="https://www.linkedin.com/in/abhijit-biswas-3b6586162/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <BsLinkedin />
-            </a>
-
-            {/* Github */}
-            <a
-              href="https://github.com/abhijitnr"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <BsGithub />
-            </a>
-          </div>
-
-          {/* Direct contant me using mail */}
-          <div className="contact_form">
-            <form ref={form} onSubmit={sendEmail}>
-              <label>
-                Name <span>*</span>
-              </label>
-              <br />
-              <input type="text" name="user_name" placeholder="Name" required />
-              <br />
-              <label>
-                Email <span>*</span>
-              </label>
-              <br />
-              <input
-                type="email"
-                name="user_email"
-                placeholder="Email"
-                required
-              />
-              <br />
-              <label>
-                Message <span>*</span>
-              </label>
-              <br />
-              <textarea name="message" placeholder="Message" />
-              <br />
-              <input
-                className="send_button"
-                type="submit"
-                value="Send"
-                required
-              />
-            </form>
+          {/* Contact image */}
+          <div>
+            <img src={contact} alt="Contact" />
           </div>
         </div>
 
-        {/* Contact image */}
-        <div>
-          <img src={contact} alt="Contact" />
-        </div>
-      </div>
-
-      <p>
-        Designed and Developed by <span>Abhijit Biswas</span>, &copy; Copyright
-        2023. All rights reserved.
-      </p>
-    </section>
+        <p>
+          Designed and Developed by <span>Abhijit Biswas</span>, &copy;
+          Copyright 2023. All rights reserved.
+        </p>
+      </section>
+      <ToastContainer />
+    </>
   );
 };
 
